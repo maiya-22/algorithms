8a5e7491f743c8a0f14efaacd725cc87acc65a7b
maiya-22 <maiya_o@yahoo.com>
2019-04-07 00:46:29 -0400

    saving progress

--- reduce.js
+++ reduce.js
@@ -1,7 +1,33 @@
 // re-create Array.reduce functionality
 
-const reduce = (arr, func, combinator) => {
+// VERSION ONE: simple function
+
+// const reduce = (arr, func, combinator) => {
+//     combinator = combinator ? combinator : 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         let item = arr[i];
+//         let index = i;
+//         combinator = func(item, combinator, index);
+//     }
+//     return combinator;
+// }
+
+// let result = reduce(
+//     arr,
+//     (item, combinator, index) => {
+//         combinator[item] = true;
+//         return combinator;
+//     },
+//     {});
+
+// console.log('result:', result);
+
+
+// VERSION TWO: put on the Array.prototype:
+
+Array.prototype.reduce = function (func, combinator) {
     combinator = combinator ? combinator : 0;
+    let arr = this;
     for (let i = 0; i < arr.length; i++) {
         let item = arr[i];
         let index = i;
@@ -10,12 +36,12 @@ const reduce = (arr, func, combinator) => {
     return combinator;
 }
 
-let result = reduce(
-    arr,
-    (item, combinator, index) => {
-        combinator[item] = true;
-        return combinator;
-    },
-    {});
 
-console.log('result:', result);
+
+console.log('result:',
+    [1, 2, 3, 4, 5, 6]
+        .reduce((item, combinator, index) => {
+            combinator[item] = true;
+            return combinator;
+        },
+            {}));
